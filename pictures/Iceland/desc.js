14e528cb9d47ffeac42b0c5842b167c6042c82d0
jonathom <jonathanbahlmann@web.de>
2021-04-13 00:05:53 +0200

    titles

---
+++ pictures/Iceland/desc.js
@@ -0,0 +1,67 @@
+iceland_desc = {
+    "nr": 14,
+    "desc": [
+        {
+            "nr": 1,
+            "title": "Skeiðará",
+            "text": "Skeiðará is a 30km long river originating from the melting of the glacier Skeiðarárjökull."
+        },
+        {
+            "nr": 2,
+            "title": "Akureyri"
+        },
+        {
+            "nr": 3,
+            "title": "Skeiðarárjökull"
+        },
+        {
+            "nr": 4,
+            "title": "Myvatn",
+            "text": "A Lake."
+        },
+        {
+            "nr": 5,
+            "title": "Vulcanic Plains"
+        },
+        {
+            "nr": 6,
+            "title": "Hydroelectric Dams"
+        },
+        {
+            "nr": 7,
+            "title": ""
+        },
+        {
+            "nr": 8,
+            "title": "River from Glacier Eyjafjallajökull"
+        },
+        {
+            "nr": 9,
+            "title": "Hofsjökull"
+        },
+        {
+            "nr": 10,
+            "title": ""
+        },
+        {
+            "nr": 11,
+            "title": ""
+        },
+        {
+            "nr": 12,
+            "title": "South of Eyjafjallajökull"
+        },
+        {
+            "nr": 13,
+            "title": "Vesturland"
+        },
+        {
+            "nr": 14,
+            "title": "Vesturland"
+        }
+    ]
+}
+
+pantanal_desc = {
+    
+}
