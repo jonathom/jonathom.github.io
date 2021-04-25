920e706e9092a57997f4555acf51567303f72285
jonathom <jonathanbahlmann@web.de>
2021-04-25 20:40:18 +0200

    descriptions

--- descriptions/iceland_desc.js
+++ descriptions/iceland_desc.js
@@ -4,61 +4,73 @@ iceland_desc = {
     "desc": [
         {
             "nr": 1,
-            "title": "Skeiðará",
-            "text": "Skeiðará is a 30km long river originating from the melting of the glacier Skeiðarárjökull."
+            "title": "Glacier Melting",
+            "text": "Skeiðará is a 30km long river originating from the melting of the glacier Skeiðarárjökull, in the South of Iceland. Displayed here in true color."
         },
         {
             "nr": 2,
-            "title": "Akureyri"
+            "title": "Akureyri",
+            "text": "Akureyri is a town at the Fjord Eyjafjörður. It is seen here in a false color composite highlighting vegetation in red. Snow is white and water black."
         },
         {
             "nr": 3,
-            "title": "Skeiðarárjökull"
+            "title": "Skeiðarárjökull",
+            "text": "This glacier is the origin of the river seen in picture 1. Melting lakes can be seen in the south. This is a true colored image, with the root drawn twice from original data to account for the huge range in reflectance values from bare land, glacier ice and snow."
         },
         {
             "nr": 4,
             "title": "Myvatn",
-            "text": "A Lake."
+            "text": "A Lake in the northwest of Iceland. This is a composite of Optical infrared and Radar data. So roughly: Vegetation is green, bare soil goes into blue, and topographic features are highlighted in red."
         },
         {
             "nr": 5,
-            "title": "Vulcanic Plains"
+            "title": "Vulcanic Plains",
+            "text": "For this image, visible red and unvisible infrared channels have been combined. As plants would be displayed in green we can see that most of the area is only bare stone."
         },
         {
             "nr": 6,
-            "title": "Hydroelectric Dams"
+            "title": "Hydroelectric Dams",
+            "text": "Two rivers worth of melting water from the glacier Vatnajökull are dammed up for electricity."
         },
         {
             "nr": 7,
-            "title": ""
+            "title": "Rivers",
+            "text": "In this combination of (infra)red channels we observe patterns of vegetation and bare rock."
         },
         {
             "nr": 8,
-            "title": "River from Glacier Eyjafjallajökull"
+            "title": "River from Glacier Eyjafjallajökull",
+            "text": ""
         },
         {
             "nr": 9,
-            "title": "Hofsjökull"
+            "title": "Hofsjökull",
+            "text": "A big glacier in the midwest of the Island. Observed in mostly infratred channels, we are able to tell the threshold between fresher snow and ice flowing out from under it."
         },
         {
             "nr": 10,
-            "title": ""
+            "title": "Snow",
+            "text": "Mountain ridges."
         },
         {
             "nr": 11,
-            "title": ""
+            "title": "No title",
+            "text": ""
         },
         {
             "nr": 12,
-            "title": "South of Eyjafjallajökull"
+            "title": "South of Eyjafjallajökull",
+            "text": "Clouds can be seen at the foot of the glacier."
         },
         {
             "nr": 13,
             "title": "Vesturland"
+            "text": "Seen here in true color."
         },
         {
             "nr": 14,
-            "title": "Vesturland"
+            "title": "Vesturland",
+            "text": "Seen here in false color (infrared)."
         }
     ]
 }
