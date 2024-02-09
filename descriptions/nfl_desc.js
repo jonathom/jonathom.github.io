bf69785320576bd975c1b62e725df976ab499a0d
jonathom <jonathanbahlmann@web.de>
2024-02-09 22:28:27 +0100

    Add newfoundland collection

---
+++ descriptions/nfl_desc.js
@@ -0,0 +1,50 @@
+nfl_desc = {
+    "nr": 9,
+    "col_desc": "With the beginning of the new year 2024, the coast of Canada's province Newfoundland and Labrador is the scene of a magnificent mechanism. Strong winds force freshly built ice along the coast and out to sea, drawing stringy patterns into the ocean.",
+    "desc": [{
+            "nr": 1,
+            "title": "#1",
+            "text": ""
+        },
+        {
+            "nr": 2,
+            "title": "#2",
+            "text": ""
+        },
+        {
+            "nr": 3,
+            "title": "#3",
+            "text": ""
+        },
+        {
+            "nr": 4,
+            "title": "#4",
+            "text": ""
+        },
+        {
+            "nr": 5,
+            "title": "#5",
+            "text": ""
+        },
+        {
+            "nr": 6,
+            "title": "#6",
+            "text": ""
+        },
+        {
+            "nr": 7,
+            "title": "#7",
+            "text": ""
+        },
+        {
+            "nr": 8,
+            "title": "#8",
+            "text": ""
+        },
+        {
+            "nr": 9,
+            "title": "#9",
+            "text": ""
+        }
+    ]
+}
