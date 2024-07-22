908e852a53c579173362e8a578d4460642e991ed
jonathom <jonathanbahlmann@web.de>
2024-07-22 22:14:18 +0200

    Add Melt collection

---
+++ descriptions/melt_desc.js
@@ -0,0 +1,55 @@
+melt_desc = {
+    "nr": 10,
+    "col_desc": "Well into the summer on Earth's Northern Hemisphere, the ice holding tight the Canadian Arctic coast is finally, fatally weakened by the sun. The whiteout breaks into a Myriad of lakes. Ice floes the size of small towns disolve into the warming sea. The desert blooms. The ground is frozen rock solid year-round.",
+    "desc": [{
+            "nr": 1,
+            "title": "#1",
+            "text": ""
+        },
+        {
+            "nr": 2,
+            "title": "#2",
+            "text": ""
+        },
+        {
+            "nr": 3,
+            "title": "#3",
+            "text": ""
+        },
+        {
+            "nr": 4,
+            "title": "#4",
+            "text": ""
+        },
+        {
+            "nr": 5,
+            "title": "#5",
+            "text": ""
+        },
+        {
+            "nr": 6,
+            "title": "#6",
+            "text": ""
+        },
+        {
+            "nr": 7,
+            "title": "#7",
+            "text": ""
+        },
+        {
+            "nr": 8,
+            "title": "#8",
+            "text": ""
+        },
+        {
+            "nr": 9,
+            "title": "#9",
+            "text": ""
+        },
+        {
+            "nr": 10,
+            "title": "#10",
+            "text": ""
+        }
+    ]
+}
